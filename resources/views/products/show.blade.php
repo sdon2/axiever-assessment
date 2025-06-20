66262d85b1dcd3646093a0523e2390c356bd8c11
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2025-06-20 10:11:50 +0530

    Added view product an changed button names

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -2,10 +2,10 @@
 
     <x-slot:header>
         <li class="nav-item d-none d-md-block"><a href="{{ url('/products') }}" class="nav-link">Products</a></li>
-        <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">Edit Product</a></li>
+        <li class="nav-item d-none d-md-block"><a href="#" class="nav-link">View Product</a></li>
     </x-slot>
 
-    @section('title', 'Edit Product')
+    @section('title', 'View Product')
 
     <!--begin::App Content-->
     <div class="app-content">
@@ -14,51 +14,36 @@
             <!--begin::Row-->
             <div class="row">
                 <div class="card">
-                    <div class="card-header">Edit Product</div>
+                    <div class="card-header">View Product</div>
                     <div class="card-body">
-                        <form method="POST" action="{{ route('products.update', $product->id) }}">
-                            @csrf
-                            @method('PUT')
                             <div class="row">
                                 <div class="col-md-6">
                                     <label for="name" class="form-label">Product Name</label>
                                     <input type="text" class="form-control" id="name" name="name"
-                                        value="{{ old('name', $product->name) }}" required>
-                                    @error('name')
-                                        <div class="is-invalid text-danger">{{ $message }}</div>
-                                    @enderror
+                                        value="{{ old('name', $product->name) }}" required readonly>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="sku" class="form-label">SKU</label>
                                     <input type="text" class="form-control" id="sku" name="sku"
-                                        value="{{ old('sku', $product->sku) }}" required>
-                                    @error('sku')
-                                        <div class="is-invalid text-danger">{{ $message }}</div>
-                                    @enderror
+                                        value="{{ old('sku', $product->sku) }}" required readonly>
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-md-6">
                                     <label for="price" class="form-label">Price</label>
                                     <input type="number" class="form-control" id="price" name="price"
-                                        step="0.01" value="{{ old('price', $product->price) }}" required>
-                                    @error('price')
-                                        <div class="is-invalid text-danger">{{ $message }}</div>
-                                    @enderror
+                                        step="0.01" value="{{ old('price', $product->price) }}" required readonly>
                                 </div>
                                 <div class="col-md-6">
                                     <label for="stock" class="form-label">Current Stock</label>
                                     <input type="text" class="form-control" id="stock" name="stock"
-                                        value="{{ old('stock', $product->stock) }}" required>
-                                    @error('stock')
-                                        <div class="is-invalid text-danger">{{ $message }}</div>
-                                    @enderror
+                                        value="{{ old('stock', $product->stock) }}" required readonly>
                                 </div>
                             </div>
                             <div class="row mt-3">
                                 <div class="col-md-6">
-                                    <button type="submit" class="btn btn-primary">Save Product</button>
                                     <a href="{{ route('products.index') }}" class="btn btn-secondary">Back</a>
+                                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning">Edit Product</a>
                                 </div>
                             </div>
                         </form>
