196552ba65e7e01cf9ff2afa5c9392f223a5e1dd
Saravanakumar Arumugam <saravanakumar.a.o@gmail.com>
2025-06-20 18:48:05 +0530

    Increase stock on sales order delete

--- app/Http/Controllers/SalesOrderController.php
+++ app/Http/Controllers/SalesOrderController.php
@@ -1,12 +1,13 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use App\DataTables\SalesOrdersDataTable;
 use App\Models\SalesOrder;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Yajra\DataTables\Facades\DataTables;
 
 class SalesOrderController extends Controller
@@ -18,34 +19,34 @@ class SalesOrderController extends Controller
             $data = SalesOrder::query();
 
             return DataTables::of($data)
-                    ->addIndexColumn()
-                    ->addColumn('Customer', 'customer_name')
-                    ->editColumn('order_date', function ($row) {
-                        return Carbon::parse($row->order_date)->format('d/m/Y');
-                    })
-                    ->editColumn('created_at', function ($row) {
-                        return Carbon::parse($row->created_at)->diffForHumans();
-                    })
-                    ->editColumn('items', function ($row) {
-                        return $row->products->sum('quantity');
-                    })
-                    ->addColumn('status', function ($row) {
-                        switch ($row->status) {
-                            case 'pending':
-                                return '<span class="badge text-dark bg-warning">Pending</span>';
-                            case 'completed':
-                                return '<span class="badge text-dark bg-success">Completed</span>';
-                            case 'cancelled':
-                                return '<span class="badge text-dark bg-danger">Cancelled</span>';
-                            default:
-                                return '<span class="badge text-dark bg-secondary">Unknown</span>';
-                        }
-                    })
-                    ->addColumn('actions', function ($row) {
-                        return view('salesOrders.actions', compact('row'));
-                    })
-                    ->rawColumns(['status','actions'])
-                    ->make(true);
+                ->addIndexColumn()
+                ->addColumn('Customer', 'customer_name')
+                ->editColumn('order_date', function ($row) {
+                    return Carbon::parse($row->order_date)->format('d/m/Y');
+                })
+                ->editColumn('created_at', function ($row) {
+                    return Carbon::parse($row->created_at)->diffForHumans();
+                })
+                ->editColumn('items', function ($row) {
+                    return $row->products->sum('quantity');
+                })
+                ->addColumn('status', function ($row) {
+                    switch ($row->status) {
+                        case 'pending':
+                            return '<span class="badge text-dark bg-warning">Pending</span>';
+                        case 'completed':
+                            return '<span class="badge text-dark bg-success">Completed</span>';
+                        case 'cancelled':
+                            return '<span class="badge text-dark bg-danger">Cancelled</span>';
+                        default:
+                            return '<span class="badge text-dark bg-secondary">Unknown</span>';
+                    }
+                })
+                ->addColumn('actions', function ($row) {
+                    return view('salesOrders.actions', compact('row'));
+                })
+                ->rawColumns(['status', 'actions'])
+                ->make(true);
         }
         return $dataTable->render('salesOrders.index');
     }
@@ -73,8 +74,24 @@ class SalesOrderController extends Controller
 
     public function destroy(SalesOrder $salesOrder)
     {
-        $salesOrder->delete();
+        try {
+            DB::beginTransaction();
 
-        return redirect()->route('salesOrders.index')->with('success', 'Sales Order deleted successfully.');
+            if ($salesOrder->status == 'completed') {
+                foreach ($salesOrder->products as $product) {
+                    $product->product->increment('stock', $product->quantity);
+                }
+            }
+
+            $salesOrder->delete();
+
+            DB::commit();
+
+            return redirect()->route('salesOrders.index')->with('success', 'Sales Order deleted successfully.');
+
+        } catch (Exception $e) {
+            DB::rollBack();
+            return back()->with('error', 'Failed to delete Sales Order: ' . $e->getMessage());
+        }
     }
 }
